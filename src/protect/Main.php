988ffde861b37a87d06421838fe113d557fdd003
DaisukeDaisuke <17798680+DaisukeDaisuke@users.noreply.github.com>
2021-07-28 17:11:25 +0900

    Disables the plugin and adds a warning.<This plugin will not work.>

--- src/protect/Main.php
+++ src/protect/Main.php
@@ -27,6 +27,18 @@ class Main extends PluginBase{
 	}*/
 
 	public function onEnable(){
+
+		$this->getLogger()->warning("This plugin is in the process of being created and will not work.");
+		$this->getLogger()->notice("I recommend using the following plugins.");
+		$this->getLogger()->notice("iProtector: https://poggit.pmmp.io/p/iProtector/");
+		$this->getLogger()->notice("WorldProtect: https://poggit.pmmp.io/p/WorldProtect");
+		$this->getLogger()->notice("WorldGuard: https://poggit.pmmp.io/p/WorldGuard");
+		$this->getLogger()->error("Disable this plugin.");
+
+		$this->getServer()->getPluginManager()->disablePlugin($this);
+		return;
+
+
 		$list = new Config($this->getDataFolder()."list.yml", Config::YAML, []);
 		$permissions = new Config($this->getDataFolder()."permissions.yml", Config::YAML, []);
 		$this->setting = new Config($this->getDataFolder()."setting.yml", Config::YAML, [
@@ -62,9 +74,9 @@ class Main extends PluginBase{
 		$this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
 		$this->getServer()->getPluginManager()->registerEvents(new RangeEventListener($setting), $this);
 
-		$this->getServer()->getCommandMap()->registerAll("protect", [
+		/*$this->getServer()->getCommandMap()->registerAll("protect", [
 			new BuyLandCommand($this, $lang->translate("command.BuyLandCommand._name"), $lang->translate("command.BuyLandCommand._description")),
-		]);
+		]);*/
 
 		range::setPricePerBlock($setting->get("Price Per Block", 0.1));
 		//$lang->get("language", ["pos" => ["reset" => "The coordinate data has been deleted."]])
